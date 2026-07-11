3783626b8618f202efb766480623b7f3edf68523
RizkyArdiansyahhh <rizky.ardiansyahhh06@gmail.com>
2026-07-11 19:49:19 +0700

    feat: implement product module with Prisma integration, global validation pipes, and configuration management

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductModule } from './module/product/product.module';
+import { PrismaModule } from './prisma/database/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    ProductModule, PrismaModule
+],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
