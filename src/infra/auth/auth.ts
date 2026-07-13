aeb4754deb59938a1e69f6f6c950a2f4016c2320
Rizky Ardiansyah <126098455+RizkyArdiansyahhh@users.noreply.github.com>
2026-07-14 02:58:13 +0700

    db:products

--- src/infra/auth/auth.ts
+++ src/infra/auth/auth.ts
@@ -8,11 +8,19 @@ console.log("FRONTEND_URL =", process.env.FRONTEND_URL);
 const prisma = new PrismaService();
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL,
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
   emailAndPassword: {
     enabled: true,
+  },
+  socialProviders: {
+    google: {
+      prompt: "select_account",
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }
   },
   trustedOrigins: ["http://localhost:3000"],
 });
